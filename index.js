54f6a7a15ea4644d09db0c2fd3996aa9d9c5b41d
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-02-01 12:39:45 +0530

    about signed cookie

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const usersRoute = require('./routes/users.js')
 const postsRoute = require('./routes/posts.js')
 const cookieParser = require('cookie-parser')
 
-app.use(cookieParser())
+app.use(cookieParser("secretcode"))
+
+app.get('/signedCookies',(req,res)=>{
+    res.cookie("made-in","India",{signed:true})
+    res.send('signed cookies')
+})
+app.get('/verify',(req,res)=>{
+    console.log(req.signedCookies)
+    res.send("verified")
+})
 
 app.get('/getcookies',(req,res)=>{
     res.cookie("name","vikram")
     res.cookie("greet","Jai shree Ram")
     res.send("get your cookies")
 })
-
+app.get('/greet',(req,res)=>{
+    const{name} = req.cookies
+    res.send(`hii,${name}`)
+})
 app.get('/',(req,res)=>{
     console.dir(req.cookies)
     res.send('i am root route')
